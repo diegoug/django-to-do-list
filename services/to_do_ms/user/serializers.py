1caa61113968a4a846c3156b0509cf5d89b2ba9d
DiegoUG <diego.uribe.gamez@gmail.com>
2021-12-20 20:59:04 -0500

    [add] serializer and views

---
+++ services/to_do_ms/user/serializers.py
@@ -0,0 +1,78 @@
+from django.contrib.auth import password_validation, authenticate
+
+from oauth2_provider.models import Application
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from profiles.models import User
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email']
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        min_length=8, max_length=64)
+
+    def validate(self, data):
+        user = authenticate(username=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError('The credentials are not valid')
+
+        self.context['user'] = user
+        return data
+
+    def create(self, data):
+        oauth_data = {
+            'user': self.context['user'],
+            'authorization_grant_type': 'password'
+        }
+
+        if not Application.objects.filter(**oauth_data).exists():
+            oauth_application = Application(**oauth_data)
+            oauth_application.save()
+
+        oauth2_app = Application.objects.get(**oauth_data)
+
+        return self.context['user'], oauth2_app.client_secret, oauth2_app.client_id
+
+
+class UserSignUpSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        min_length=4,
+        max_length=20,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    first_name = serializers.CharField(
+        min_length=2, max_length=50)
+    last_name = serializers.CharField(
+        min_length=2, max_length=100)
+
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    password = serializers.CharField(
+        min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(
+        min_length=8, max_length=64)
+
+    def validate(self, data):
+        passwd = data['password']
+        passwd_conf = data['password_confirmation']
+        if passwd != passwd_conf:
+            raise serializers.ValidationError('Passwords do not match')
+        password_validation.validate_password(passwd)
+
+        return data
+
+    def create(self, data):
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        return user
---
+++ services/to_do_ms/user/views.py
@@ -0,0 +1,58 @@
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasReadWriteScope
+
+from rest_framework import status, viewsets, filters, mixins
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from profiles.models import User
+
+from .serializers import UserModelSerializer, UserUpdateModelSerializer, UserLoginSerializer, UserSignUpSerializer
+
+class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, 
+                  mixins.UpdateModelMixin, mixins.DestroyModelMixin, 
+                  viewsets.GenericViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+    
+    authentication_classes = [OAuth2Authentication]
+    
+    search_fields = ['email', 'first_name', 'last_name']
+    filter_backends = [filters.SearchFilter]
+
+    lookup_field = 'email'
+    lookup_value_regex = '[\w.@+-]+'
+    
+    # custom actions ----------------------------------------------------------
+    def get_permissions(self):
+        if 'post' in self.action_map:
+            if self.action_map['post'] in ['signup', 'login']:
+                return []
+        return super(UserViewSet, self).get_permissions()
+    
+    def get_serializer_class(self):
+        if self.action == 'signup':
+            return UserSignUpSerializer
+        if self.action == 'login':
+            return UserLoginSerializer
+        return super(UserViewSet, self).get_serializer_class()
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, client_secret, client_id = serializer.save()
+        data = {
+            'user': UserModelSerializer(user).data,
+            'client_secret': client_secret,
+            'client_id': client_id
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
+        return Response(data, status=status.HTTP_201_CREATED)
